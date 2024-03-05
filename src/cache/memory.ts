4adbb93753e690acf76732d075e88191cb55e6a9
Kanahiro <mediterranean1769@gmail.com>
2024-03-05 12:24:47 +0900

    feat: add ttl to memory cache

--- src/cache/memory.ts
+++ src/cache/memory.ts
@@ -1,14 +1,30 @@
+import MemoryCache from 'memory-cache-node';
+
 import { type Cache, type Value } from './index.js';
 
-const MEMORY_CACHE_KVS: Record<string, Value> = {};
-const memoryCache: () => Cache = function () {
+const itemsExpirationCheckIntervalInSecs = 60; // check every 60 seconds
+const maxItemCount = 1000; // 500KB * 1000 items = 500MB
+const MEMORY_CACHE_KVS = new MemoryCache.MemoryCache<string, Value>(
+    itemsExpirationCheckIntervalInSecs,
+    maxItemCount,
+);
+
+type MemoryCacheOptions = {
+    ttl: number;
+    maxItemCount: number;
+};
+
+const memoryCache: (options: MemoryCacheOptions) => Cache = function (
+    options: MemoryCacheOptions,
+) {
     return {
         name: 'memory',
         set: async function (key: string, value: Value) {
-            MEMORY_CACHE_KVS[key] = value;
+            MEMORY_CACHE_KVS.storeExpiringItem(key, value, options.ttl);
         },
         get: async function (key: string): Promise<Value | undefined> {
-            return (MEMORY_CACHE_KVS[key] as Value) ?? undefined;
+            const item = MEMORY_CACHE_KVS.retrieveItemValue(key);
+            return item;
         },
     };
 };
