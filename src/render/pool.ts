ba8bcfef47b93f4495111454387eaa3383acb768
Kanahiro <mediterranean1769@gmail.com>
2024-05-07 10:23:23 +0900

    improve: optimize async-await

--- src/render/pool.ts
+++ src/render/pool.ts
@@ -7,37 +7,31 @@ import type { Cache } from '../cache/index.js';
 
 // key:value = styleJsonString:Pooled Map Instance
 const mapPoolDict: Record<string, genericPool.Pool<mbgl.Map>> = {};
-async function getRenderPool(
+function getRenderPool(
     styleUrl: string,
     cache: Cache,
     mode: 'tile' | 'static',
 ) {
     const dictKey = `${styleUrl}-${mode}`;
     if (mapPoolDict[dictKey] === undefined) {
-        const styleJsonBuf = await getSource(styleUrl, cache);
-        if (styleJsonBuf === null) throw new Error('style not found');
-        const style = JSON.parse(styleJsonBuf.toString()) as StyleSpecification;
+        const styleJsonBuf = getSource(styleUrl, cache);
         const pool = genericPool.createPool({
             create: async () => {
                 const map = new mbgl.Map({
                     request: function (req, callback) {
                         getSource(req.url, cache)
                             .then((buf) => {
-                                if (buf === null) {
-                                    callback();
-                                    return;
-                                }
                                 callback(undefined, { data: buf });
                             })
                             .catch((err: any) => {
-                                callback(err);
+                                callback(err, { data: null });
                             });
                     },
                     ratio: 1,
                     // @ts-ignore
                     mode,
                 });
-                map.load(style);
+                map.load(JSON.parse((await styleJsonBuf).toString()));
                 return map;
             },
             destroy: async (map: mbgl.Map) => {
