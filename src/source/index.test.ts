65374617e4f3a71445cb7faa31e085fd2644ca03
Kanahiro <mediterranean1769@gmail.com>
2024-05-25 23:14:30 +0900

    addtest

---
+++ src/source/index.test.ts
@@ -0,0 +1,37 @@
+import { describe, it, expect, vi } from 'vitest';
+
+import { getSource } from './index.js';
+
+describe('getSource', () => {
+    it('file://', async () => {
+        const uri = 'file://localdata/style.json';
+        const data = await getSource(uri);
+        expect(data).not.toBeNull();
+    });
+
+    it('https://', async () => {
+        const uri = 'https://demotiles.maplibre.org/style.json';
+        const data = await getSource(uri);
+        expect(data).not.toBeNull();
+    });
+
+    it('mbtiles://', async () => {
+        const uri = 'mbtiles://localdata/school.mbtiles/0/0/0';
+        const data = await getSource(uri);
+        expect(data).not.toBeNull();
+    });
+
+    it('pmtiles://', async () => {
+        const uri = 'mbtiles://localdata/school.mbtiles/0/0/0';
+        const data = await getSource(uri);
+        expect(data).not.toBeNull();
+    });
+
+    it('invalid uri', async () => {
+        const uri = 'invalid://localdata/style.json';
+        const data = await getSource(uri);
+        expect(data).toBeNull();
+    });
+
+    // s3://
+});
