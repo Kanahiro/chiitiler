fd878717380dffc3b5b10f05759ad6c07a30296b
Kanahiro <mediterranean1769@gmail.com>
2024-06-09 15:12:18 +0900

    refactor: stop too much DRY

--- src/server/index.ts
+++ src/server/index.ts
@@ -1,5 +1,7 @@
 import { Hono } from 'hono';
 import { serve } from '@hono/node-server';
+import { stream } from 'hono/streaming';
+import { type Sharp } from 'sharp';
 import {
     type StyleSpecification,
     validateStyleMin,
@@ -7,10 +9,20 @@ import {
 
 import { type Cache } from '../cache/index.js';
 import { getDebugPage, postDebugPage } from './debug.js';
-import { tileResponse } from './response.js';
+import { renderTilePipeline, type SupportedFormat } from '../render/index.js';
 
 function isValidStylejson(stylejson: any): stylejson is StyleSpecification {
     return validateStyleMin(stylejson).length === 0;
+}
+
+function isValidXyz(x: number, y: number, z: number) {
+    if (x < 0 || y < 0 || z < 0) return false;
+    if (x >= 2 ** z || y >= 2 ** z) return false;
+    return true;
+}
+
+function isSupportedFormat(ext: string): ext is SupportedFormat {
+    return ['png', 'jpeg', 'jpg', 'webp'].includes(ext);
 }
 
 type InitServerOptions = {
@@ -30,24 +42,90 @@ function initServer(options: InitServerOptions) {
     hono.get('/tiles/:z/:x/:y_ext', async (c) => {
         const url = c.req.query('url') ?? null;
         if (url === null) return c.body('url is required', 400);
-        const res = await tileResponse(c, {
-            mode: 'url',
-            cache: options.cache,
-            url,
+
+        // path params
+        const z = Number(c.req.param('z'));
+        const x = Number(c.req.param('x'));
+        let [_y, ext] = c.req.param('y_ext').split('.');
+        const y = Number(_y);
+
+        if (!isValidXyz(x, y, z)) return c.body('invalid xyz', 400);
+        if (!isSupportedFormat(ext)) return c.body('invalid format', 400);
+
+        // query params
+        const tileSize = Number(c.req.query('tileSize') ?? 512);
+        const quality = Number(c.req.query('quality') ?? 100);
+        const margin = Number(c.req.query('margin') ?? 0);
+
+        let pipeline: Sharp;
+        try {
+            pipeline = await renderTilePipeline({
+                stylejson: url,
+                z,
+                x,
+                y,
+                tileSize,
+                cache: options.cache,
+                margin,
+                ext,
+                quality,
+            });
+        } catch (e) {
+            console.error(`render error: ${e}`);
+            return c.body('failed to render tile', 400);
+        }
+
+        c.header('Content-Type', `image/${ext}`);
+        return stream(c, async (stream) => {
+            for await (const chunk of pipeline) {
+                stream.write(chunk);
+            }
         });
-        return res;
     });
 
     hono.post('/tiles/:z/:x/:y_ext', async (c) => {
         // body
         const { style } = await c.req.json();
         if (!isValidStylejson(style)) return c.body('invalid stylejson', 400);
-        const res = await tileResponse(c, {
-            mode: 'style',
-            cache: options.cache,
-            style,
+
+        // path params
+        const z = Number(c.req.param('z'));
+        const x = Number(c.req.param('x'));
+        let [_y, ext] = c.req.param('y_ext').split('.');
+        const y = Number(_y);
+
+        if (!isValidXyz(x, y, z)) return c.body('invalid xyz', 400);
+        if (!isSupportedFormat(ext)) return c.body('invalid format', 400);
+
+        // query params
+        const tileSize = Number(c.req.query('tileSize') ?? 512);
+        const quality = Number(c.req.query('quality') ?? 100);
+        const margin = Number(c.req.query('margin') ?? 0);
+
+        let pipeline: Sharp;
+        try {
+            pipeline = await renderTilePipeline({
+                stylejson: style,
+                z,
+                x,
+                y,
+                tileSize,
+                cache: options.cache,
+                margin,
+                ext,
+                quality,
+            });
+        } catch (e) {
+            console.error(`render error: ${e}`);
+            return c.body('failed to render tile', 400);
+        }
+
+        c.header('Content-Type', `image/${ext}`);
+        return stream(c, async (stream) => {
+            for await (const chunk of pipeline) {
+                stream.write(chunk);
+            }
         });
-        return res;
     });
 
     return {
