971da30c66df948b994a9b3d85d53d41860b8234
Kanahiro <mediterranean1769@gmail.com>
2024-06-11 22:04:07 +0900

    fix: use async function

--- src/render/pool.ts
+++ src/render/pool.ts
@@ -6,28 +6,14 @@ import type { StyleSpecification } from '@maplibre/maplibre-gl-style-spec';
 
 import { getSource } from '../source/index.js';
 import type { Cache } from '../cache/index.js';
+import { getTransparentBuffer } from './sharp.js';
 
 const EMPTY_BUFFER = Buffer.alloc(0);
-const TRANSPARENT_BUFFER: Record<string, Buffer> = {
-    // 1x1 transparent images
-    png: Buffer.from(
-        'iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAYAAAAfFcSJAAAADUlEQVR4nGP4//8/AwAI/AL+p5qgoAAAAABJRU5ErkJggg==',
-        'base64',
-    ),
-    webp: Buffer.from(
-        'UklGRkAAAABXRUJQVlA4WAoAAAAQAAAAAAAAAAAAQUxQSAIAAAAAAFZQOCAYAAAAMAEAnQEqAQABAAFAJiWkAANwAP789AAA',
-        'base64',
-    ),
-    jpeg: Buffer.from(
-        '/9j/4AAQSkZJRgABAQAAAQABAAD/2wBDAAgGBgcGBQgHBwcJCQgKDBQNDAsLDBkSEw8UHRofHh0aHBwgJC4nICIsIxwcKDcpLDAxNDQ0Hyc5PTgyPC4zNDL/2wBDAQkJCQwLDBgNDRgyIRwhMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjIyMjL/wAARCAABAAEDASIAAhEBAxEB/8QAHwAAAQUBAQEBAQEAAAAAAAAAAAECAwQFBgcICQoL/8QAtRAAAgEDAwIEAwUFBAQAAAF9AQIDAAQRBRIhMUEGE1FhByJxFDKBkaEII0KxwRVS0fAkM2JyggkKFhcYGRolJicoKSo0NTY3ODk6Q0RFRkdISUpTVFVWV1hZWmNkZWZnaGlqc3R1dnd4eXqDhIWGh4iJipKTlJWWl5iZmqKjpKWmp6ipqrKztLW2t7i5usLDxMXGx8jJytLT1NXW19jZ2uHi4+Tl5ufo6erx8vP09fb3+Pn6/8QAHwEAAwEBAQEBAQEBAQAAAAAAAAECAwQFBgcICQoL/8QAtREAAgECBAQDBAcFBAQAAQJ3AAECAxEEBSExBhJBUQdhcRMiMoEIFEKRobHBCSMzUvAVYnLRChYkNOEl8RcYGRomJygpKjU2Nzg5OkNERUZHSElKU1RVVldYWVpjZGVmZ2hpanN0dXZ3eHl6goOEhYaHiImKkpOUlZaXmJmaoqOkpaanqKmqsrO0tba3uLm6wsPExcbHyMnK0tPU1dbX2Nna4uPk5ebn6Onq8vP09fb3+Pn6/9oADAMBAAIRAxEAPwD3+iiigD//2Q==',
-    ),
-};
 
 function handleFileExt(uri: string) {
     // extract extension only, take into account query string or hash
     const basename = path.basename(uri).split(/[?#]/)[0];
-    const ext = basename.split('.').pop();
-    if (ext === undefined) return null;
+    const ext = basename.split('.')[1];
     const l = ext.toLowerCase();
     if (l === 'jpeg') return 'jpg';
     return l;
@@ -45,27 +31,17 @@ async function getRenderPool(
         const pool = genericPool.createPool({
             create: async () => {
                 const map = new mbgl.Map({
-                    request: function (req, callback) {
-                        const ext = handleFileExt(req.url);
-                        getSource(req.url, cache)
-                            .then((buf) => {
-                                if (buf) {
-                                    callback(undefined, { data: buf });
-                                } else if (ext && TRANSPARENT_BUFFER[ext])
-                                    callback(undefined, {
-                                        data: TRANSPARENT_BUFFER[ext],
-                                    });
-                                else
-                                    callback(undefined, { data: EMPTY_BUFFER });
-                            })
-                            .catch(() => {
-                                if (ext && TRANSPARENT_BUFFER[ext])
-                                    callback(undefined, {
-                                        data: TRANSPARENT_BUFFER[ext],
-                                    });
-                                else
-                                    callback(undefined, { data: EMPTY_BUFFER });
-                            });
+                    request: async function (req, callback) {
+                        const buf = await getSource(req.url, cache);
+                        if (buf) {
+                            callback(undefined, { data: buf });
+                        } else {
+                            const trnBuf = await getTransparentBuffer(
+                                handleFileExt(req.url),
+                            );
+                            if (trnBuf) callback(undefined, { data: trnBuf });
+                            else callback(undefined, { data: EMPTY_BUFFER });
+                        }
                     },
                     ratio: 1,
                     // @ts-ignore
