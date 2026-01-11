c19295926d303aee96b0de254575dccf1bbd8892
Kanahiro <mediterranean1769@gmail.com>
2026-01-11 23:13:56 +0900

    refactor: simplify codes

--- src/server/index.ts
+++ src/server/index.ts
@@ -17,39 +17,40 @@ type InitServerOptions = {
 
 type InitializedServer = {
 	app: Hono;
-	tiles: Hono;
-	clip: Hono;
 	start: () => void;
 };
 
 function initServer(options: InitServerOptions): InitializedServer {
-	const tiles = createTilesRouter({
-		cache: options.cache,
-		stream: options.stream,
-	});
-	const clip = createClipRouter({
-		cache: options.cache,
-		stream: options.stream,
-	});
-	const camera = createCameraRouter({
-		cache: options.cache,
-		stream: options.stream,
-	});
-
 	const hono = new Hono();
 	if (options.debug) {
 		hono.get('/debug', getDebugPage);
 		hono.get('/editor', getEditorgPage);
 	}
 	hono.get('/health', (c) => c.text('OK'));
-	hono.route('/camera', camera);
-	hono.route('/tiles', tiles);
-	hono.route('/', clip);
+	hono.route(
+		'/static',
+		createCameraRouter({
+			cache: options.cache,
+			stream: options.stream,
+		}),
+	);
+	hono.route(
+		'/tiles',
+		createTilesRouter({
+			cache: options.cache,
+			stream: options.stream,
+		}),
+	);
+	hono.route(
+		'/',
+		createClipRouter({
+			cache: options.cache,
+			stream: options.stream,
+		}),
+	);
 
 	return {
 		app: hono,
-		tiles,
-		clip,
 		start: () => {
 			const server = serve({ port: options.port, fetch: hono.fetch });
 			process.on('SIGINT', () => {
